5de6a5e682b021a3005992f80ecafeb115c93a9b
Mustafa Dane <mustafadane@gmail.com>
2019-04-09 18:19:14 -0500

    comments are added

--- citiesByPopulation.js
+++ citiesByPopulation.js
@@ -1,27 +1,40 @@
 var readline = require('readline')
 
+//This is a way of reading input at node applications.
 var RI = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-
+//Everytime there is a new line entered, this callback calculator will fire.
 RI.on('line', function(line) {
     calculator(line)
 })
 
+//Data object to save all the data read from input.
+//Keys are populations.
+//Values are an array of cities with that population.
 const data = {}
+
+//Populations array to easily sort by populatin later.
 const populations = []
 
 function calculator(line){
+    //Skip the line if it starts with '#'
     if(line[0] === '#') return
 
+    //Line is splitted by '|' for every line entered.
     const lineArr = line.split('|')
+
+    //An object representing this city is created.
     const cityObj = {
         city: lineArr[1],
         state: lineArr[2],
         interstates: lineArr[3].split(';')
     }
+
+    //If data has the same population already, push the city object to the array.
+    //Else new key is created by the population number and value is an array with one element which is the city object. Also this unique population is pushed to the populations array and the array is sorted.
     if(data.hasOwnProperty(lineArr[0])) {
         data[lineArr[0]].push(cityObj)
     } else {
@@ -30,7 +43,12 @@ function calculator(line){
         populations.sort((a,b) => b-a)
     }
 
+    //Every time this calculation happens, we output the new results.
     console.log("\nNEW RESULTS:\n")
+
+    //For each population (pop) in the populations array:
+    //First population is printed,
+    //Then the city information is printed in the expected format.
     populations.forEach(pop => {
         console.log(pop + '\n')
         data[pop].forEach(city => {
@@ -42,14 +60,7 @@ function calculator(line){
 
 }
 
+//Comparator function is used to sort interstates.
 function comparator(interstateA, interstateB) {
     return Number(interstateA.slice(2, interstateA.indexOf(' '))) - Number(interstateB.slice(2, interstateB.indexOf(' ')))
 }
-
-/*
-# Skip this line.
-27|Chicago|Illinois|I-94;I-90;I-88;I-57;I-55
-83|New York|New York|I-78;I-95;I-87;I-80
-15|Phoenix|Arizona|I-10;I-17;I-8
-15|Philadelphia|Pennsylvania|I-95;I-76
-*/
--- interstatesByCity.js
+++ interstatesByCity.js
@@ -1,24 +1,37 @@
 var readline = require('readline')
 
+//This is a way of reading input at node applications.
 var RI = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-
+//Everytime there is a new line entered, this callback calculator will fire.
 RI.on('line', function(line) {
     calculator(line)
 })
 
-
+//Data object to save all the data read from input.
+//Keys are the interstate names.
+//Values are the number of states where this interstate passes.
 const data = {}
 
 function calculator(line) {
-    const result = []
+    //Skip the line if it starts with '#'
     if(line[0] === '#') return
 
+    //Result array is used because sorting is easier.
+    const result = []
+
+    //Line is splitted by '|' for every line entered.
     const lineArr = line.split('|')
+
+    //Then interstates splitted into its own array.
     const interstates = lineArr[3].split(';')
+
+    //Loop through the interstates:
+    //If data has the key as interstate, increment the value by one.
+    //Else create the key and set value to one.
     for(let i = 0; i < interstates.length; i++) {
         if(data.hasOwnProperty(interstates[i])) {
             data[interstates[i]]++
@@ -26,23 +39,21 @@ function calculator(line) {
             data[interstates[i]] = 1
         }
     }
+
+    //Loop through the object and push the correct string versions to result array.
     for(let key in data) {
         result.push(`${key} ${data[key]}`)
     }
 
-    console.log('New Results:\n')
+    //Every time a new line is entered, new results are calculated and printed.
+    //Before printing, interstates are sorted.
+    console.log('NEW RESULTS:\n')
     console.log(result.sort(comparator).join('\n'))
 }
 
+//Comparator function is used to sort interstates.
 function comparator(interstateA, interstateB) {
     return Number(interstateA.slice(2, interstateA.indexOf(' '))) - Number(interstateB.slice(2, interstateB.indexOf(' ')))
 }
 
 
-/*
-# Skip this line.
-4|Raleigh|North Carolina|I-40;I-85;I-95
-27|Chicago|Illinois|I-94;I-90;I-88;I-57;I-55
-2|Madison|Wisconsin|I-94;I-90
-
-*/
